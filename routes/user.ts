e0842415bb8848a6a5ea5bd1d703e496e7c52ba8
thezx <oscarmiguelayalaruelas@gmail.com>
2021-10-25 14:07:16 -0700

    Rutas del usuario

--- routes/user.ts
+++ routes/user.ts
@@ -1,3 +1,4 @@
+import { validateJWT } from './../middlewares/validate-jwt';
 import { Router } from 'express';
 import { check } from 'express-validator';
 import {
@@ -11,7 +12,6 @@ import {
 
 import { validateFields } from '../middlewares/validate-fields';
 import { emailUserExists, userExists } from '../helpers/db-validators';
-import { validateJWT } from '../middlewares/validate-jwt';
 
 
 const router = Router();
@@ -19,23 +19,27 @@ const router = Router();
 
 router.get('/', getUsers);
 
-router.get('/search', getUsersName);
+// router.get('/search', getUsersName);
 
 router.get('/:id',[
     check('id').custom( userExists ),
     validateFields
 ], getUserId);
 
+router.post('/',[
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El correo no es válido').isEmail(),
+    check('email').custom( emailUserExists ),
+    check('password', 'La contraseña es obligatoría').not().isEmpty(),
+    check('password', 'La contraseña tiene que tener un mínimo de 6 caracteres').isLength({ min: 6 }),
+    validateFields
+], createUser);
+
 router.put('/:id',[
+    validateJWT,
     check('id').custom( userExists ),
     validateFields
 ],updateUserId);
-
-router.post('/',[
-    check('email', 'El correo no es válido').isEmail(),
-    check('email').custom( emailUserExists ),
-    validateFields
-], createUser);
 
 router.delete('/:id',[
     validateJWT,
